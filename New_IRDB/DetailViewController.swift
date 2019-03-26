0197af12d7ef8d33c4b85333b8cf7a31ca7f7d61
andreaperez <aap15a@acu.edu>
2019-03-25 22:33:48 -0500

    ok

--- New_IRDB/DetailViewController.swift
+++ New_IRDB/DetailViewController.swift
@@ -12,14 +12,58 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
+    @IBOutlet weak var studio: UILabel!
+    @IBOutlet weak var episode: UILabel!
+    @IBOutlet weak var format: UILabel!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var moviename: UILabel!
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
+//            if let label = studio{
+//                label.text = detail.studio
+//            }
+//            if let label = episode {
+//                label.text = "\(detail.episodes)"
+//            }
+//            if let label =  format{
+//                label.text = detail.format
+//            }
+//            if let label = yearLabel {
+//                label.text = detail.yearEnd
+//            }
+//            if let label = yearLabel {
+//                label.text = detail.yearStart
+//            }
+//            if let label = topLabel {
+//                label.text = detail.studio
+//            }
+//            if let label = imageURL {
+//                label.text = detail.imageURL
+//            }
+//            if let label = description {
+//                label.text = detail.description
+//            }
+//            if let label = summary{
+//                label.text = detail.summary
+//
+//            }
+            moviename.text = detail.name
+            year.text = detail.yearStart
+            format.text = detail.format
+            episode.text = "\(detail.episodes)"
+            studio.text = detail.studio
         }
+        
     }
 
     override func viewDidLoad() {
@@ -31,7 +75,7 @@ class DetailViewController: UIViewController {
     var detailItem: Entry? {
         didSet {
             // Update the view.
-            configureView()
+//            configureView()
         }
     }
 
--- New_IRDB/MasterViewController.swift
+++ New_IRDB/MasterViewController.swift
@@ -89,7 +89,7 @@ class MasterViewController: UITableViewController {
         cell.textLabel!.text = mediaName
         
         let mediaYear = (rebootDataModel?.franchise[indexPath.section].entries[indexPath.row].yearStart)!
-        cell.textLabel!.text = mediaYear
+        cell.detailTextLabel!.text = mediaYear
 
         return cell
     }
